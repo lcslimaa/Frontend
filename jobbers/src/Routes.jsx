cf47a1bb833a295c33859b8f7daccc3f55a5183b
b4iano <lucas-lima2000@Hotmail.com>
2020-07-01 14:04:59 -0300

    Institucional

--- jobbers/src/Routes.jsx
+++ jobbers/src/Routes.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Cadastro2 from './pages/Cadastro2/Cadastro2';
-import Login from './pages/Login/Login';
-import PrincipalApp from './pages/PrincipalApp/PrincipalApp';
-import CadastroPrestador from './pages/CadastroPrestador/CadastroPrestador'
-import PrincipalAppPrestador from './pages/appPrestador/PrincipalApp/PrincipalAppPrestador'
+import Cadastro2 from "./pages/Cadastro2/Cadastro2";
+import Login from "./pages/Login/Login";
+import PrincipalApp from "./pages/PrincipalApp/PrincipalApp";
+import CadastroPrestador from "./pages/CadastroPrestador/CadastroPrestador";
+import PrincipalAppPrestador from "./pages/appPrestador/PrincipalApp/PrincipalAppPrestador";
 
-export default props => 
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route from='/home' component={PrincipalApp}/>
-            <Route from='/homePrestador' component={PrincipalAppPrestador}/>
-            <Route from='/register' component={Cadastro2}/>
-            <Route from='/registerPrestador' component={CadastroPrestador}/>
-        </Switch>
-    </BrowserRouter>
+export default (props) => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route from="/home" component={PrincipalApp} />
+      <Route from="/homePrestador" component={PrincipalAppPrestador} />
+      <Route from="/register" component={Cadastro2} />
+      <Route from="/registerPrestador" component={CadastroPrestador} />
+      <Route from="/login" component={Login} />
+    </Switch>
+  </BrowserRouter>
+);
